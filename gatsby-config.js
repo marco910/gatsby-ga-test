2c28e45d0769dd9795d7917a312cc9725c5ea44c
Marco Feicht <57726854+marco910@users.noreply.github.com>
2020-11-10 16:09:03 +0100

    Removed Manifest plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,18 +16,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
-      }
-    },
-    {
       resolve: `gatsby-plugin-gdpr-cookies`,
       options: {
         googleAnalytics: {
@@ -35,9 +23,7 @@ module.exports = {
           cookieName: "gatsby-gdpr-google-analytics", // default
           anonymize: true, // default
           allowAdFeatures: false // default
-        },
-        // defines the environments where the tracking should be available  - default is ["production"]
-        environments: ["production", "development"]
+        }
       }
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
